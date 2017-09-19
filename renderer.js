61c28c9b07b4419db996ca2cdecef41bfd50b41e
Federico Fernández <federico.fernandez@rga.com>
2017-09-19 15:16:01 -0300

    Changing library

--- renderer.js
+++ renderer.js
@@ -1,28 +1,26 @@
-const raspi = require('raspi');
-const pwm = require('raspi-pwm');
+const Gpio = require('onoff').Gpio;
+const acPin = new Gpio(4, 'out');
 
 const light = document.querySelector('.light');
 const ac = document.querySelector('.ac');
 
 raspi.init(() => {
-  const acPin = new pwm.PWM('P1-3');
-  const lightPin = new pwm.PWM('P1-5');
   
   ac.addEventListener('click', () => {
     ac.classList.toggle('active');
-    if(acPin.value === 1) {
+    if(acPin.readSync() === 1) {
       acPin.write(0);
     } else {
       acPin.write(1);
     }
   });
 
-  light.addEventListener('click', () => {
+  /*light.addEventListener('click', () => {
     light.classList.toggle('active');
     if(lightPin.value === 1) {
       lightPin.write(0);
     } else {
       lightPin.write(1);
     }
-  });
+  });*/
 });
