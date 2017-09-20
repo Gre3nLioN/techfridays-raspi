775b434c7a5a6db5354a4d1f14236baddbeecf34
Federico Fernández <federico.fernandez@rga.com>
2017-09-20 15:50:02 -0300

    Package json adition

--- renderer.js
+++ renderer.js
@@ -1,26 +1,25 @@
-const Gpio = require('onoff').Gpio;
-const acPin = new Gpio(4, 'out');
+const Gpio = require('pigpio').Gpio,
 
 const light = document.querySelector('.light');
 const ac = document.querySelector('.ac');
 
-raspi.init(() => {
-  
-  ac.addEventListener('click', () => {
-    ac.classList.toggle('active');
-    if(acPin.readSync() === 1) {
-      acPin.write(0);
-    } else {
-      acPin.write(1);
-    }
-  });
+const acPin = new Gpio(17);
+const lightPin = new Gpio(4);
 
-  /*light.addEventListener('click', () => {
-    light.classList.toggle('active');
-    if(lightPin.value === 1) {
-      lightPin.write(0);
-    } else {
-      lightPin.write(1);
-    }
-  });*/
+ac.addEventListener('click', () => {
+  ac.classList.toggle('active');
+  if(acPin.digitalRead() === 1) {
+    acPin.digitalWrite(0);
+  } else {
+    acPin.digitalWrite(1);
+  }
+});
+
+light.addEventListener('click', () => {
+  light.classList.toggle('active');
+  if(lightPin.digitalRead() === 1) {
+    lightPin.digitalWrite(0);
+  } else {
+    lightPin.digitalWrite(1);
+  }
 });
